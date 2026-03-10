e284219d09aad6b1a02b41908c18b3890c5a6aaa
jimpa <jimmyparcero@protonmail.com>
2026-03-10 14:49:38 +0100

    ex00 done

---
+++ ex00/whatever.hpp
@@ -0,0 +1,29 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template<typename T>
+const T& max(T& a, T& b)
+{
+	if(a > b)
+		return a;
+	return b;
+}
+
+template<typename T>
+const T& min(T& a, T& b)
+{
+	if(a < b)
+		return a;
+	return b;
+}
+
+template<typename T>
+void swap(T& a, T& b)
+{
+	T c = a;
+	a = b;
+	b = c;
+}
+
+
+#endif
